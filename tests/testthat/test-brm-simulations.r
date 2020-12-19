527163211003bdf5f4ca3ed044e0aec24546733d
ning <ningyuan.sg@gmail.com>
2020-12-19 15:26:52 +0800

    Add tests for simulation study 2 of BRM article

--- tests/testthat/test-brm-simulations.r
+++ tests/testthat/test-brm-simulations.r
@@ -13,13 +13,16 @@ setup({
   download_urls <- list(
     "ego2hopnets_24.RData" = "https://osf.io/download/vucnf/",
     "study1_rawoutput.zip" = "https://osf.io/download/vy42b/",
+    "study2_rawoutput.zip" = "https://osf.io/download/6xzgq/",
     "24toynets.csv"        = "https://osf.io/download/9wz6s/")
-  lapply(names(download_urls), function(fn) {
-    message("Downloading ", fn, "...")
+  sapply(names(download_urls), function(fn) {
     if (!file_exists(fn)) download.file(download_urls[[fn]], fn)
   })
   # Extract zipped files
-  if (!dir_exists("study1_rawoutput")) unzip("study1_rawoutput.zip")
+  zip_files <- Filter(function(fn) grepl("\\.zip$", fn), names(download_urls))
+  sapply(zip_files, function(zfn) {
+    if (!dir_exists(gsub("\\.zip$", "", zfn))) unzip(zfn)
+  })
 })
 
 load("ego2hopnets_24.RData")
@@ -29,13 +32,34 @@ retentions <- seq(0.1, 0.9, by=0.1)
 for (retention in retentions) {
   for (i in 1:nrow(v2011)) {
     test_that(sprintf(
-      "Lexical retrieval simulation: network #%d, retention %f", i, retention), {
+      "Lexical retrieval simulation: network %d, retention %f", i, retention), {
         results <- spreadr(
           start_run=data.frame(node=v2011$name[i], activation=100),
           network=ego2hopnets_24[[i]],
           decay=0, retention=retention, suppress=0, time=10)
         expected <- read.csv(
-          sprintf("study1_rawoutput/%d_%s_%.1f.csv", i, v2011$name[i], retention),
+          sprintf(
+            "study1_rawoutput/%d_%s_%.1f.csv",
+            i, v2011$name[i], retention),
+          colClasses=c("NULL", NA, NA, NA))  # ignore the first column
+        expect_equal(results, expected)
+      }
+    )
+    test_that(sprintf(
+      "False memory simulation: network %d, retention %f", i, retention), {
+        neighbour_nodes <- igraph::neighbors(
+          ego2hopnets_24[[i]], as.character(v2011$name[i]))
+        results <- spreadr(
+          start_run=data.frame(
+            node=as.vector(neighbour_nodes$name),
+            activation=100/length(neighbour_nodes),
+            stringsAsFactors=FALSE),
+          network=ego2hopnets_24[[i]],
+          decay=0, retention=retention, suppress=0, time=10)
+        expected <- read.csv(
+          sprintf(
+            "study2_rawoutput/%d_%s_%.1f_sim2.csv",
+            i, v2011$name[i], retention),
           colClasses=c("NULL", NA, NA, NA))  # ignore the first column
         expect_equal(results, expected)
       }
